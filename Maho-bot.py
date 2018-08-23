8445736db7b7af02ded9b9e5d38486a054a4af78
Napoleon <napdemesa89@gmail.com>
2018-08-23 12:10:28 -0500

    Add $hello function

--- Maho-bot.py
+++ Maho-bot.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 
 client = discord.Client()
 
@@ -10,5 +11,15 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+    if message.content.startswith('$hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
 
 client.run(token)
