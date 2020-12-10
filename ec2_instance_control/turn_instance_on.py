b0971c4b45222786d48a2a61810835f3ca8dcd33
Napoleon de Mesa <napoleon.demesa@nielsen.com>
2020-12-09 22:12:53 -0600

    update mc server start

--- ec2_instance_control/turn_instance_on.py
+++ ec2_instance_control/turn_instance_on.py
@@ -4,6 +4,7 @@ import json
 import boto3
 from botocore.exceptions import ClientError
 from mcstatus import MinecraftServer
+from mc_server import turn_mc_server_on as mcs
 
 
 def get_instance_id(path):
@@ -68,31 +69,10 @@ def turn_instance_off(instance_id):
     return
 
 
-def check_minecraft_server_status():
-    server = MinecraftServer.lookup("3.21.241.184")
-    try:
-        status = server.status()
-        print(f"The server has {status.players.online} players and replied in {status.latency} ms")
-        mc_server_online = True
-    except Exception:
-        print('The server is currently not on.')
-        mc_server_online = False
-
-    return mc_server_online
-
-
 def main():
     instance_id = get_instance_id('minecraft')
     instance_status = check_instance_status(instance_id['INSTANCE_ID'])
-    if instance_status:
-        if check_minecraft_server_status():
-            print('minecraft server is online')
-        else:
-            print('minecraft server is offline... starting up server...')
-            os.system('java -Xmx1024M -Xms1024M -jar server.jar nogui')
-            while not check_minecraft_server_status():
-                print('waiting for server to load...')
-            check_minecraft_server_status()
+    mcs.turn_server_on(instance_status)
 
     #turn_instance_on(instance_id['INSTANCE_ID'])
     #turn_instance_off(instance_id['INSTANCE_ID'])
---
+++ mc_server/turn_mc_server_on.py
@@ -0,0 +1,28 @@
+import os
+from mcstatus import MinecraftServer
+from ec2_instance_control import turn_instance_on as tio
+
+
+def check_minecraft_server_status():
+    server = MinecraftServer.lookup("3.129.206.49")
+    try:
+        status = server.status()
+        print(f"The server has {status.players.online} players and replied in {status.latency} ms")
+        mc_server_online = True
+    except Exception:
+        print('The server is currently not on.')
+        mc_server_online = False
+
+    return mc_server_online
+
+
+def turn_server_on(instance_status):
+    if instance_status:
+        if check_minecraft_server_status():
+            print('minecraft server is online')
+        else:
+            print('minecraft server is offline... starting up server...')
+            os.system('java -Xmx1024M -Xms1024M -jar server.jar nogui')
+            while not check_minecraft_server_status():
+                print('waiting for server to load...')
+            check_minecraft_server_status()
