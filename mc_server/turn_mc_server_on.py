ace94e1c80610714847f72e03ff5face9f125824
Napoleon de Mesa <napoleon.demesa@nielsen.com>
2020-12-09 22:35:28 -0600

    update turn_mc_server.py

--- mc_server/turn_mc_server_on.py
+++ mc_server/turn_mc_server_on.py
@@ -1,4 +1,5 @@
 import os
+import time
 from mcstatus import MinecraftServer
 #from ec2_instance_control import turn_instance_on as tio
 
@@ -10,7 +11,7 @@ def check_minecraft_server_status():
         print(f"The server has {status.players.online} players and replied in {status.latency} ms")
         mc_server_online = True
     except Exception:
-        print('The server is currently not on.')
+        #print('The server is currently not on.')
         mc_server_online = False
 
     return mc_server_online
@@ -22,7 +23,12 @@ def turn_server_on(instance_status):
             print('minecraft server is online')
         else:
             print('minecraft server is offline... starting up server...')
-            os.system('java -Xmx1024M -Xms1024M -jar server.jar nogui')
+            os.system('java -Xmx1024M -Xms1024M -jar home/ec2-user/Maho-bot/mc_server/server.jar nogui')
+            idx = 0
+            animation = '.....'
             while not check_minecraft_server_status():
-                print('waiting for server to load...')
+                print(animation[idx % len(animation)], end="\r")
+                idx += 1
+                time.sleep(0.1)
+                #print('waiting for server to load...')
             check_minecraft_server_status()
