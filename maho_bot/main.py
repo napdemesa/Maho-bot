5aa137e24ecfe13f0a23707680818133d1aa6a12
Napoleon de Mesa <napoleon.demesa@nielsen.com>
2020-12-12 23:48:54 -0600

    update main.py

--- maho_bot/main.py
+++ maho_bot/main.py
@@ -1,10 +1,20 @@
+import json
+import boto3
+
 from Maho_bot import Maho
 
-def main():
-    token = input('Enter Discord bot token for Maho: ')
+def get_instance_id(path):
+    ssm = boto3.client('ssm')
+    args = {"Name": path, "WithDecryption": True}
+    param = ssm.get_parameter(**args)
 
+    return json.loads(param['Parameter']['Value'])
+
+def main():
+    #token = input('Enter Discord bot token for Maho: ')
+    creds = get_instance_id('minecraft')
     client = Maho()
-    client.run(token)
+    client.run(creds['maho_bot_token'])
 
 if __name__ == "__main__":
     main()
